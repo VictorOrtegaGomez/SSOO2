2660255374b9515b3350617d5460b1fb55582976
Victor <victor.ortega2@alu.uclm.es>
2023-02-23 14:51:43 +0100

    pc code and manager implementation (untested)

--- P1/src/manager.c
+++ P1/src/manager.c
@@ -3,63 +3,208 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define FILE_NAME "estudiantes.txt"
+#define LOG_FILE_NAME "log.txt"
+#define FIRST_LOG_LINE "******** Log del sistema ********"
+#define LAST_LOG_LINE "FIN DE PROGRAMA"
+#define PROCESS_A_SUCCESS "Creación de directorios finalizada."
+#define PROCESS_A_FAILURE "Error. Process A finished with errors\nFIN DE PROGRAMA"
+#define PROCESS_B_SUCCESS "Copia de modelos de examen, finalizada."
+#define PROCESS_B_FAILURE "Error. Process B finished with errors\nFIN DE PROGRAMA"
+#define PROCESS_C_SUCCESS "Creación de archivos con nota necesaria para alcanzar la nota de corte, finalizada."
+#define PROCESS_C_FAILURE "Error. Process C finished with errors\nFIN DE PROGRAMA"
+#define READING 0
+#define WRITING 1
+#define WRITING_PIPE_SIZE 256
 
 int main(int argc, char*argv[]){
 
-    pid_t pidA, pidB, pidC;
-    int statusA, statusBC, i;
+    pid_t pidA, pidB, pidC, pidBC;
+    int statusA, statusBC, mean, counter, logFilePointer, i;
+    int pipePointers[2];
+    char wrPipe[WRITING_PIPE_SIZE];
+    char *mark;
+
+    /*Creating and opening of Log file*/
+    if((logFilePointer = open(LOG_FILE_NAME, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1){
+        fprintf(stderr, "Error. Log file could not be created\nTerminating program\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /*The first line of the log file is written*/
+    if(write(logFilePointer, FIRST_LOG_LINE, sizeof(FIRST_LOG_LINE)) != sizeof(FIRST_LOG_LINE)){
+        fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+        exit(EXIT_FAILURE);
+    }
 
     /*Creation of the child process A*/
     if((pidA = fork()) < 0) {
         fprintf(stderr, "Error. The child process A could not be created\nTerminating program\n");
+
+        if(write(logFilePointer, stderr, sizeof(stderr)) != sizeof(stderr)){
+            fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+            exit(EXIT_FAILURE);
+        }
+
         exit(EXIT_FAILURE);
     } else if(pidA == 0) {
         execl("./exec/pa", argv[0], FILE_NAME, NULL);
         fprintf(stderr, "Error. The child process A could not be executed\nTerminating program\n");
+
+        if(write(logFilePointer, stderr, sizeof(stderr)) != sizeof(stderr)){
+            fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+            exit(EXIT_FAILURE);
+        }
+
         exit(EXIT_FAILURE);
     }
 
     /*We wait for the child process A to finish*/
     if(waitpid(pidA, &statusA, 0) == -1){
         fprintf(stderr, "Error. Waiting for child proccess A failed\nTerminating program\n");
+
+        if(write(logFilePointer, stderr, sizeof(stderr)) != sizeof(stderr)){
+            fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+            exit(EXIT_FAILURE);
+        }
+
         exit(EXIT_FAILURE);
+    }
+
+    /*We write the results of process A execution*/
+    if(statusA == EXIT_SUCCESS){
+        if(write(logFilePointer, PROCESS_A_SUCCESS, sizeof(PROCESS_A_SUCCESS)) != sizeof(PROCESS_A_SUCCESS)){
+            fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        if(write(logFilePointer, PROCESS_A_FAILURE, sizeof(PROCESS_A_FAILURE)) != sizeof(PROCESS_A_FAILURE)){
+            fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+            exit(EXIT_FAILURE);
+        }
     }
 
     /*Creation of child process B*/
     if((pidB = fork()) < 0) {
         fprintf(stderr, "Error. The child process B could not be created\nTerminating program\n");
+
+        if(write(logFilePointer, stderr, sizeof(stderr)) != sizeof(stderr)){
+            fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+            exit(EXIT_FAILURE);
+        }
+
         exit(EXIT_FAILURE);
     } else if(pidB == 0) {
         execl("./exec/pb", argv[0], FILE_NAME, NULL);
         fprintf(stderr, "Error. The child process B could not be executed\nTerminating program\n");
-        exit(EXIT_FAILURE);
-    }
 
-    /*Creation of child process C
-    if((pidC = fork()) < 0) {
-        fprintf(stderr, "Error. The child process C could not be created\nTerminating program\n");
-        exit(EXIT_FAILURE);
-    } else if(pidC == 0) {
-        execl("./exec/pc", argv[0], FILE_NAME, NULL);
-        fprintf(stderr, "Error. The child process C could not be executed\nTerminating program\n");
-        exit(EXIT_FAILURE);
-    }
-    */
-
-    /*We wait for the child processes B and C to finish
-    for(i = 0; i < 2; i++){
-        if(wait(&statusBC) == -1){
-            fprintf(stderr, "Error. Waiting for child proccess B or C failed\n");
+        if(write(logFilePointer, stderr, sizeof(stderr)) != sizeof(stderr)){
+            fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
             exit(EXIT_FAILURE);
         }
-    }
-    */
 
-   if(waitpid(pidB, &statusBC, 0) == -1){
-            fprintf(stderr, "Error. Waiting for child proccess B or C failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /*Comunication Pipe between manager and C proccess*/
+    if(pipe(pipePointers) == -1){
+        fprintf(stderr, "Error. Pipe could not be created\nTerminating program\n");
+
+        if(write(logFilePointer, stderr, sizeof(stderr)) != sizeof(stderr)){
+            fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
             exit(EXIT_FAILURE);
+        }
+
+        exit(EXIT_FAILURE);
+    }
+
+    sprintf(wrPipe, "%d", pipePointers[WRITING]);
+
+    /*Creation of child process C*/
+    if((pidC = fork()) < 0) {
+        fprintf(stderr, "Error. The child process C could not be created\nTerminating program\n");
+
+        if(write(logFilePointer, stderr, sizeof(stderr)) != sizeof(stderr)){
+            fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+            exit(EXIT_FAILURE);
+        }
+
+        exit(EXIT_FAILURE);
+    } else if(pidC == 0) {
+        execl("./exec/pc", wrPipe, FILE_NAME, NULL);
+        fprintf(stderr, "Error. The child process C could not be executed\nTerminating program\n");
+
+        if(write(logFilePointer, stderr, sizeof(stderr)) != sizeof(stderr)){
+            fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+            exit(EXIT_FAILURE);
+        }
+
+        exit(EXIT_FAILURE);
+    }
+
+    /*We read process C marks and calculate the mean*/
+    counter = 0;
+    mean = 0;
+    mark = 0;
+
+    while(*mark != EOF){
+        if(read(pipePointers[READING], mark, sizeof(mark)) < 0){
+                fprintf(stderr, "Error. Pipe could not be read\nTerminating program\n");
+
+                if(write(logFilePointer, stderr, sizeof(stderr)) != sizeof(stderr)){
+                    fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+                    exit(EXIT_FAILURE);
+                }
+
+                exit(EXIT_FAILURE);
+        }
+        mean += atoi(mark);
+        counter++;
+    }
+
+    mean = mean/counter;
+    
+    /*We wait for the child processes B and C to finish*/
+    for(i = 0; i < 2; i++){     
+        if((pidBC = wait(&statusBC)) == -1){
+            if(pidBC == pidB){
+                if(statusBC == EXIT_SUCCESS){
+                    if(write(logFilePointer, PROCESS_B_SUCCESS, sizeof(PROCESS_B_SUCCESS)) != sizeof(PROCESS_B_SUCCESS)){
+                        fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+                        exit(EXIT_FAILURE);
+                    }
+                }else{
+                    if(write(logFilePointer, PROCESS_B_FAILURE, sizeof(PROCESS_B_FAILURE)) != sizeof(PROCESS_B_FAILURE)){
+                        fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+            } else if(pidBC == pidC){
+                if(statusBC == EXIT_SUCCESS){
+                    if(write(logFilePointer, PROCESS_C_SUCCESS, sizeof(PROCESS_C_SUCCESS)) != sizeof(PROCESS_C_SUCCESS)){
+                        fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+                        exit(EXIT_FAILURE);
+                    }
+                }else{
+                    if(write(logFilePointer, PROCESS_C_FAILURE, sizeof(PROCESS_C_FAILURE)) != sizeof(PROCESS_C_FAILURE)){
+                        fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+
+            }else if(pidBC == -1){
+                fprintf(stderr, "Error. Waiting for child proccesses B and C failed\nTerminating program\n");
+
+                if(write(logFilePointer, stderr, sizeof(stderr)) != sizeof(stderr)){
+                    fprintf(stderr, "Error. Log file could not be written\nTerminating program\n");
+                    exit(EXIT_FAILURE);
+                }
+            } 
+            exit(EXIT_FAILURE);
+        }
     }
 
     exit(EXIT_SUCCESS);
---
+++ P1/src/pc.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define MAXIMUM_LINE_SIZE 100
+#define MAXIMUM_NAME_SIZE 4098
+#define MAIN_DIRECTORY_NAME "estudiantes"
+#define TXT_NAME "result.txt"
+#define RESULT_FILE_PERMISSIONS 0644
+#define RESULT_SENTENCE "La nota que debes obtener en este nuevo examen para superar la prueba es "
+#define READING 0
+#define WRITING 1
+
+void writeResultFiles(int writingPipePointer, const char *fileName){
+
+    DIR *dp;
+    FILE *file;
+    int resultFilePointer, i;
+    char line[MAXIMUM_LINE_SIZE];
+    char directoryName[MAXIMUM_NAME_SIZE];
+    char resultFileName[MAXIMUM_NAME_SIZE];
+    char resultSentence[sizeof(RESULT_SENTENCE) + 2];
+    char mark;
+    char *token;
+
+    /*We open the file*/
+    if((file = fopen(fileName, "r")) == NULL){
+        fprintf(stderr,"Error. File could not be open\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /*File reading by lines*/
+    while (fgets(line, MAXIMUM_LINE_SIZE, file))
+    {
+        /*We open the directories from the students with the first word from the lines we read*/
+        token = strtok(line, " ");
+        sprintf(directoryName, "%s/%s", MAIN_DIRECTORY_NAME, token);
+
+        /*We check that the folders exist*/
+        if ((dp = opendir(directoryName)) == NULL) {
+            fprintf(stderr, "Error. Directory %s does not exist\n", MAIN_DIRECTORY_NAME);
+        }else{
+            /*We get the mark*/
+            for(i = 0; i < 2; i++) token = strtok(NULL, " ");
+
+            /*We comunicate the old mark to the manager process*/
+            if(write(writingPipePointer, token, sizeof(token)) != sizeof(token)){
+                fprintf(stderr, "Error. Pipe could not be written\n");
+            }
+
+            /*We calculate the mark is needed*/
+            mark = 10 - atoi(token);
+
+            /*We create the file where the mark needed will be written*/
+            strcpy(resultFileName, directoryName);
+            strcat(resultFileName, "/");
+            strcat(resultFileName, TXT_NAME);
+
+            if((resultFilePointer = open(resultFileName, O_WRONLY | O_CREAT | O_TRUNC, RESULT_FILE_PERMISSIONS)) == -1){
+                fprintf(stderr, "Error. File %s could not be created\n", resultFileName);
+            }
+
+            /*We write on the file*/
+            strcpy(resultSentence, RESULT_SENTENCE);
+            strcat(resultSentence, mark);
+            
+            if(write(resultFilePointer, resultSentence, sizeof(resultSentence)) != sizeof(resultSentence)){
+                fprintf(stderr, "Error. File %s could not be written\n", resultFileName);
+            }
+        }
+        
+    }
+
+    fclose(file);
+    closedir(dp);
+    close(resultFilePointer);
+}
+
+int main(int argc, char* argv[]){
+
+    /*It is checked that we have all the arguments we need. File name must be an argument*/
+    if(argc != 2){
+        fprintf(stderr,"Error. Only file name must be introduced\n");
+        exit(EXIT_FAILURE);
+    }
+
+    writeResultFiles(atoi(argv[0]),argv[1]);
+
+    exit(EXIT_SUCCESS);
+}
