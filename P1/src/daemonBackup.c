0c6d6fd4858977fdb31bf18391a68b922baa0fcb
Victor <victor.ortega2@alu.uclm.es>
2023-03-07 17:04:06 +0100

    daemon backup process working with alarm signal

--- P1/src/daemonBackup.c
+++ P1/src/daemonBackup.c
@@ -7,9 +7,11 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <time.h>
+#include <signal.h>
 
 #define MAX_PATH 4098
 #define MAX_DATE 70
+#define TIME_FOR_BACKUP 60
 
 int getCurrentTime (char* fecha){
 
@@ -26,12 +28,12 @@ int getCurrentTime (char* fecha){
         return(-1);
     }
 
-    sprintf(fecha, "%d-%d-%d", localTime->tm_mday, localTime->tm_mon + 1, localTime->tm_year + 1900);
+    sprintf(fecha, "%d-%d-%d-%d:%d:%d", localTime->tm_mday, localTime->tm_mon + 1, localTime->tm_year + 1900, localTime->tm_hour, localTime->tm_min, localTime->tm_sec);
 
     return 1;
 }
 
-void backup (const char *path, const char *backupDirectory){
+int backup (const char *path, const char *backupDirectory){
 	DIR *dp;
 	struct dirent *node;
 	struct stat statBuf, statBufAux;
@@ -40,12 +42,12 @@ void backup (const char *path, const char *backupDirectory){
 
 	if(stat(path, &statBuf) == -1){
 		fprintf(stderr, "Error. %s could not be open\n", path);
-		exit(EXIT_FAILURE);
+        return 0;
 	}
 
 	if ((dp = opendir(path)) == NULL) {
 		fprintf(stderr, "Error. Directory %s could not be open\n", path);
-		exit(EXIT_FAILURE);
+        return 0;
 	}
 
 	while ((node = readdir(dp)) != NULL){
@@ -63,16 +65,17 @@ void backup (const char *path, const char *backupDirectory){
 		else{
 			if(link(name, newName) == -1){
 				fprintf(stderr, "Error. File %s could not be copied\n", name);
-				exit(EXIT_FAILURE);
+                return 0;   
 			}
 			
 		}		
 	}
 
 	closedir(dp);
+    return 1;
 }
 
-int main(int argc, char*argv[]){
+void signalHandler(int sig){
     
     struct stat statBuf;
     char date[MAX_DATE];
@@ -80,32 +83,41 @@ int main(int argc, char*argv[]){
     char aux[MAX_PATH];
     char *currentDirectory;
 
+    signal(SIGALRM, signalHandler);
+
     currentDirectory = getcwd(NULL, 0); /*We get the path of the current directory which is where the project will be located*/
 
     if(stat(currentDirectory, &statBuf) == -1){
-        fprintf(stderr, "Error. %s could not be open\n", argv[1]);
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Error. %s could not be open\n", currentDirectory);
     }
 
     if(!S_ISDIR(statBuf.st_mode)){
-        fprintf(stderr, "Error. %s is not a directory\n", argv[1]);
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Error. %s is not a directory\n", currentDirectory);
     }
 
     if(getCurrentTime(date) == -1){ /*We get current time*/
         fprintf(stderr, "Error. Current date could not be obtained\n");
-        exit(EXIT_FAILURE);
     }
 
     sprintf(aux, "%s", getenv("HOME"));
     sprintf(backupPath, "%s/%s", aux, date); /*We create the path of the backup directory which will be named the date we've just got*/
 
     if(mkdir(backupPath, 0777) == -1){
-        fprintf(stderr, "Error. Directory %s could not be created\n", argv[1]);
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "Error. Directory %s could not be created\n", backupPath);
     }
 
-	backup(currentDirectory, backupPath);
-	
-	exit(EXIT_SUCCESS);
+    fprintf(stdout, "Starting backup procedure...\n");
+
+	if(backup(currentDirectory, backupPath) == 0) fprintf(stderr, "Error. Backup procedure could not be completed\n");
+    else fprintf(stdout, "Backup procedure completed\n");
+    
+}
+
+int main(int argc, char* argv[]){
+
+    signal(SIGALRM, signalHandler);
+    while (1){
+        alarm(TIME_FOR_BACKUP);
+        pause();
+    }
 }
