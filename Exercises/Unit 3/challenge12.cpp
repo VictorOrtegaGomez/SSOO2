5a9d8f81ba776a48676e9712f2a10fa9f70b2d4f
victor <victor.ortega2@alu.uclm.es>
2023-04-18 11:21:13 +0200

    added information

--- Exercises/Unit 3/challenge12.cpp
+++ Exercises/Unit 3/challenge12.cpp
@@ -5,7 +5,9 @@
 #include <queue>
 #include <chrono>
 
-/*pasar el semáforo en la petición para desbloquearlo luego desde el sistema de pago
+/*
+pasar el semáforo en la petición para desbloquearlo luego desde el sistema de pago
 las peticiones llegan a una cola y con su variable de condición comprueba si la cola está vacía. cuando se envie la solicitud a la cola se hará el notify_one
-crear estructura que encapsule la peticion
-para pasar el semáforo usar punteros (mirar punteros inteligentes en c++)*/
+crear estructura que encapsule la peticion (id hilo, saldo, semaforo)
+para pasar el semáforo usar punteros (mirar punteros inteligentes en c++)
+*/
