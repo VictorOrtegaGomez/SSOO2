37895f45a4970d39a01fbdfa008e373bcdf02d3f
victor <victor.ortega2@alu.uclm.es>
2023-04-18 11:14:46 +0200

    challenge12 (only with guidence written, no code)

---
+++ Exercises/Unit 3/challenge12.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <queue>
+#include <chrono>
+
+/*pasar el semáforo en la petición para desbloquearlo luego desde el sistema de pago
+las peticiones llegan a una cola y con su variable de condición comprueba si la cola está vacía. cuando se envie la solicitud a la cola se hará el notify_one
+crear estructura que encapsule la peticion
+para pasar el semáforo usar punteros (mirar punteros inteligentes en c++)*/
