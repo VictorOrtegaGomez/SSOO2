0adcf083dc7802707cefaf03380f7a1e727292d3
victor <victor.ortega2@alu.uclm.es>
2023-04-18 14:31:48 +0200

    challenge12 working

--- Exercises/Unit 3/challenge12.cpp
+++ Exercises/Unit 3/challenge12.cpp
@@ -4,10 +4,62 @@
 #include <thread>
 #include <queue>
 #include <chrono>
+#include <ctime>
 
-/*
-pasar el semáforo en la petición para desbloquearlo luego desde el sistema de pago
-las peticiones llegan a una cola y con su variable de condición comprueba si la cola está vacía. cuando se envie la solicitud a la cola se hará el notify_one
-crear estructura que encapsule la peticion (id hilo, saldo, semaforo)
-para pasar el semáforo usar punteros (mirar punteros inteligentes en c++)
-*/
+struct request{
+    int id;
+    int balance;
+    std::mutex *semaphore;
+};
+
+
+std::queue <request*> requests;
+std::condition_variable cv;
+
+void paymentService(){
+    std::mutex mtx;
+        
+    while(1){
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, []{return !requests.empty();});
+        request *req = requests.front();
+        requests.pop();
+        req->balance -= 100;
+        std::cout <<  "Id Cliente: " << req->id << " Saldo: " << req->balance << std::endl;
+        req->semaphore->unlock();
+    }
+}
+
+void sendRequest(int id, int balance, int sleepTime){
+    std::mutex mtx;
+    mtx.lock();
+
+    srand(time(NULL));
+
+    request req;
+    req.id = id;
+    req.balance = balance;
+    req.semaphore = &mtx;
+
+    while(req.balance > 0){
+        requests.push(&req);
+        cv.notify_one();
+        mtx.lock();
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));
+    }
+
+}
+
+int main(){
+    std::thread t_paymentService(paymentService);
+    std::thread t_sendRequest1(sendRequest, 1, 1000, 3000);
+    std::thread t_sendRequest2(sendRequest, 2, 2000, 2000);
+    std::thread t_sendRequest3(sendRequest, 3, 3000, 1000);
+
+    t_paymentService.join();
+    t_sendRequest1.join();
+    t_sendRequest2.join();
+    t_sendRequest3.join();
+
+    return 0;
+}
