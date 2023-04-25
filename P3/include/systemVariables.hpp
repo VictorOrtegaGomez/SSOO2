9902f9c583829a1e3a39ff3ffffa10f9e95ada08
victor <victor.ortega2@alu.uclm.es>
2023-04-25 10:07:43 +0200

    search words added

--- P3/include/systemVariables.hpp
+++ P3/include/systemVariables.hpp
@@ -1,3 +1,4 @@
 #define NON_PREMIUM_USER_WORD_LIMIT 5
 #define USER_TYPES {"limitedPremium", "unlimitedPremium", "nonPremium"}
 #define CONCURRENT_SEARCH_REQUESTS 10
+#define SEARCH_WORDS {"castillo", "david", "ley", "vencer", "mar", "sueño", "oro", "hasta", "casa", "cielo"}
