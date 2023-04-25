c7ad583dc66d19226a66e37472a2a8d1e74db8c9
victor <victor.ortega2@alu.uclm.es>
2023-04-25 11:20:32 +0200

    main sleep time between client creation added

--- P3/include/systemVariables.hpp
+++ P3/include/systemVariables.hpp
@@ -2,3 +2,5 @@
 #define USER_TYPES {"limitedPremium", "unlimitedPremium", "nonPremium"}
 #define CONCURRENT_SEARCH_REQUESTS 10
 #define SEARCH_WORDS {"castillo", "david", "ley", "vencer", "mar", "sueño", "oro", "hasta", "casa", "cielo"}
+#define MAX_CLIENT_CREATION 5
+#define MAIN_SLEEP_TIME 3000
